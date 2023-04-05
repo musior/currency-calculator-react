d29a56bd638f43f9dc370f7eac53462e72df1cf4
Mateusz <108770502+musior@users.noreply.github.com>
2023-04-06 00:57:00 +0200

    Add logo image, create header component

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+const Header = () => {
+  return (
+    <header id="header">
+      <div className="header-logo">
+        <img src="" alt="Currency converter logo" />
+      </div>
+      <div className="header-info">
+        <h1 className="header-info-para">
+          Currency converter - this is a small application that converts a given
+          currency into PLN based on current exchange rates.
+        </h1>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
