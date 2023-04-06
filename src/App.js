ef93a03a32599ea83b57fcaf112d441945ddb75f
Mateusz <108770502+musior@users.noreply.github.com>
2023-04-06 14:31:42 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
+import Form from './components/Form';
+import Select from './components/Select';
+import Button from './components/Button';
 
 function App() {
   return (
     <>
       <Header />
-      <Main />
+      <Main>
+        <Form>
+          <Select></Select>
+          <Button></Button>
+        </Form>
+      </Main>
     </>
   );
 }
