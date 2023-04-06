a259ae17c6fea3eab2ac051705fb52ce475dc287
Mateusz <108770502+musior@users.noreply.github.com>
2023-04-06 14:29:12 +0200

    Update Main.jsx

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,25 +1,7 @@
-const Main = () => {
+const Main = ({ children }) => {
   return (
     <main id="main">
-      <form className="main-form">
-        <input
-          type="number"
-          name="value"
-          id="value"
-          placeholder="Value..."
-          required
-          step="0.01"
-          min="0.01"
-        />
-        <select id="main-select">
-          <option value="eur">EUR</option>
-          <option value="usd">USD</option>
-          <option value="chf">CHF</option>
-        </select>
-        <button id="main-submit" className="btn" type="submit">
-          Calculate
-        </button>
-      </form>
+      {children}
       <div className="main-currency-convertion"></div>
     </main>
   );
