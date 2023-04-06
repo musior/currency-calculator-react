7d465e5c7a6a6c56343893ac8d1dcf157dcf01ef
Mateusz <108770502+musior@users.noreply.github.com>
2023-04-06 14:27:49 +0200

    Create Select.jsx

---
+++ src/components/Select.jsx
@@ -0,0 +1,11 @@
+const Select = () => {
+  return (
+    <select id="main-select">
+      <option value="eur">EUR</option>
+      <option value="usd">USD</option>
+      <option value="chf">CHF</option>
+    </select>
+  );
+};
+
+export default Select;
