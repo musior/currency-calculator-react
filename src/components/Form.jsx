a8dc4de2ab7be58c5e817bd4865b5070434da932
Mateusz <108770502+musior@users.noreply.github.com>
2023-04-23 13:15:32 +0200

    Update Form.jsx
    Import Button and Select components to the Form. Remove "children" props and replace it with two props - "setResult" and "setIsLoading". Create a function "calculatedCurrency" that retrieves user input (value and selected currency) and use fetch to get current currency curse and after that calculate user input multiply by current currency curse.

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,18 +1,42 @@
-import { calculatedCurrency } from "../utils/calculatedCurrency";
+import Button from "./Button";
+import Select from "./Select";
 
-const Form = ({ children }) => {
+const Form = ({ setResult, setIsLoading }) => {
+  const calculatedCurrency = (e) => {
+    e.preventDefault();
+    console.log(e.target.userInput.value);
+    const selectCurrency = e.target.currencySelect.value;
+    const userInput = e.target.userInput.value;
+
+    const url = `https://api.nbp.pl/api/exchangerates/rates/a/${selectCurrency}/`;
+    setIsLoading(true);
+
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        const currentCurrencyCurse = data.rates[0].mid;
+        // eslint-disable-next-line no-useless-concat
+        const calculate = Number(
+          Math.round(`${currentCurrencyCurse * userInput}` + "e+2") + "e-2"
+        );
+        setIsLoading(false);
+        setResult(calculate);
+      })
+      .catch((err) => console.log(err));
+  };
   return (
     <form className="main-form" onSubmit={calculatedCurrency}>
       <input
         type="number"
-        name="value"
+        name="userInput"
         id="value"
         placeholder="Value..."
         required
         step="0.01"
         min="0.01"
       />
-      {children}
+      <Select />
+      <Button />
     </form>
   );
 };
