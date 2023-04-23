ca8bf91436340197150d2fc84d48ccebc66e48e4
Mateusz <108770502+musior@users.noreply.github.com>
2023-04-23 12:33:51 +0200

    Update Select.jsx
    Add name attribute.

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,6 +1,6 @@
 const Select = () => {
   return (
-    <select id="main-select">
+    <select id="main-select" name="currencySelect">
       <option value="eur">EUR</option>
       <option value="usd">USD</option>
       <option value="chf">CHF</option>
