69144adf6a0fcdb42de5d0a0ffd4de89b8e7df04
Mateusz <108770502+musior@users.noreply.github.com>
2023-04-23 12:49:55 +0200

    Update Main.jsx
    Remove "children" props.
    Add to the Main component From and Loader components, create two functions to handle set result and to handle loader display (SetIsLoading) and set state of result and loader using "useState" hook. Then push this two functions to the Form as a arguments.

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,8 +1,25 @@
-const Main = ({ children }) => {
+import { useState } from "react";
+import Form from "./Form";
+import Loader from "./Loader";
+
+const Main = () => {
+  const [result, setResult] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handleSetResult = (result) => {
+    setResult(result);
+  };
+
+  const handleSetIsLoading = (isLoading) => {
+    setIsLoading(isLoading);
+  };
+
   return (
     <main id="main">
-      {children}
-      <div className="main-currency-convertion"></div>
+      <Form setResult={handleSetResult} setIsLoading={handleSetIsLoading} />
+      <div className="main-currency-convertion">
+        {isLoading ? <Loader /> : <p>It's {result} PLN.</p>}
+      </div>
     </main>
   );
 };
