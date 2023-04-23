91fb379b80f8d58444d2b8147824e6bcae49e0a6
Mateusz <108770502+musior@users.noreply.github.com>
2023-04-23 12:35:18 +0200

    Update App.js
    Remove Form, Select and Button componets from App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import './App.css';
 import Header from './components/Header';
 import Main from './components/Main';
-import Form from './components/Form';
-import Select from './components/Select';
-import Button from './components/Button';
 
 function App() {
   return (
     <>
       <Header />
-      <Main>
-        <Form>
-          <Select></Select>
-          <Button></Button>
-        </Form>
-      </Main>
+      <Main />
     </>
   );
 }
